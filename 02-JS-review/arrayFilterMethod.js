835b04275cb2ccfa781364e2ffb0a9dce493f4e3
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-05-23 00:13:17 +0200

    feat: filter books by pages, movie adaptation and genre

--- 02-JS-review/arrayFilterMethod.js
+++ 02-JS-review/arrayFilterMethod.js
@@ -1,2 +1,20 @@
 import { getBooks } from "./service/bookService.js";
-import { getTotalReviewCount } from "./utils/bookUtils.js";
+
+const books = getBooks();
+
+// Filter books with more than 500 pages
+// and that also have movie adaptations
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+
+console.log(longBooksWithMovie);
+
+// Filter books that contain the "adventure" genre
+// and extract only their titles
+console.log("===== ADVENTURE BOOKS =====");
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+
+console.log(adventureBooks);
