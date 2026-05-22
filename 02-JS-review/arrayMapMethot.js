9392de26fff6b52ee2c2303fcbdba1e7ca4a22b7
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-05-22 23:49:00 +0200

    feat: practice array transformations using map

---
+++ 02-JS-review/arrayMapMethot.js
@@ -0,0 +1,20 @@
+import { getBooks } from "./service/bookService.js";
+import { getTotalReviewCount } from "./utils/bookUtils.js";
+
+const books = getBooks();
+
+// map() creates a new array by transforming each element
+const doubledNumbers = [1, 2, 3, 4, 5, 6].map((el) => el * 2);
+console.log(doubledNumbers);
+
+// Extracting only book titles
+const titles = books.map((book) => book.title);
+console.log("Titles:", titles);
+
+// Creating a simplified array of objects
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+}));
+
+console.log(JSON.stringify(essentialData, null, 2));
