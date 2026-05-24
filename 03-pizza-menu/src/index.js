61d4f614e2a237ad1193b96a03bbc8adb5a02dc9
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-05-24 19:24:33 +0200

    feat: add initial styling and layout structure for pizza menu

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 const pizzaData = [
   {
     name: "Focaccia",
@@ -48,7 +50,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,18 +59,24 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -77,10 +85,12 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen)
+  console.log(isOpen);
 
   return (
-    <footer>{new Date().toLocaleTimeString()}. We're currently open! </footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're currently open!{" "}
+    </footer>
   );
 }
 
@@ -89,7 +99,7 @@ function Pizza() {
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
 
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
