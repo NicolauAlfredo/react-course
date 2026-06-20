11eaebd2c93efb9033b91ba36bb582036df815f2
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 17:31:12 +0200

    feat: add new friends to application state

--- 06-eat-n-split/src/App.jsx
+++ 06-eat-n-split/src/App.jsx
@@ -4,19 +4,26 @@ import FriendsList from "./components/FriendsList";
 import FormAddFriend from "./components/FormAddFriend";
 import Button from "./components/Button";
 import FormSpliteBill from "./components/FormSpliteBill";
+import { initialFriends } from "./data/initialFriends";
 
 function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
   }
 
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}{" "}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
