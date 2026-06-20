805e303958aee24ea2f37ca66e58bcd57a151960
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 14:32:37 +0200

    feat: add service options data source

---
+++ 23-challenges/tip-calculator/src/data/serviceOptions.js
@@ -0,0 +1,6 @@
+export const serviceOptions = [
+  { label: "Dissatisfied", value: 0 },
+  { label: "It was okay", value: 5 },
+  { label: "It was good", value: 10 },
+  { label: "Absolutely amazing!", value: 20 },
+];
