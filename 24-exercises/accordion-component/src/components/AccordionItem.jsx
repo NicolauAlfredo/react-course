5964ec0c3b2c9419f3cd6b86a0c332a10f6c7012
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 12:17:48 +0200

    feat: create reusable AccordionItem component

---
+++ 24-exercises/accordion-component/src/components/AccordionItem.jsx
@@ -0,0 +1,11 @@
+export default function AccordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">-</p>
+
+      <div className="content-box">{text}</div>
+    </div>
+  );
+}
