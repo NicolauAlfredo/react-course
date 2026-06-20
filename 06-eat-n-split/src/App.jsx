60cdb19aa5e65f85dc417945193667adc3d749c6
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 17:04:18 +0200

    feat: add friend form toggle with reusable button actions

--- 06-eat-n-split/src/App.jsx
+++ 06-eat-n-split/src/App.jsx
@@ -6,12 +6,20 @@ import Button from "./components/Button";
 import FormSpliteBill from "./components/FormSpliteBill";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <Button>Add Friend</Button>
-        <FormAddFriend />
+        {showAddFriend && <FormAddFriend />}{" "}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
 
       <FormSpliteBill />
