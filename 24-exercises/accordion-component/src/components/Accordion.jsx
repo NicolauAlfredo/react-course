420342ab2afb321e9ae46c373b9ad3d9d2886d4c
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 12:23:11 +0200

    feat: add accordion component and FAQ data structure

--- 24-exercises/accordion-component/src/components/Accordion.jsx
+++ 24-exercises/accordion-component/src/components/Accordion.jsx
@@ -1,3 +1,12 @@
-export default function Accordion() {
-  return <div>TODO</div>;
+import AccordionItem from "./AccordionItem";
+import { faqs } from "../data/faqs";
+
+export default function Accordion({ data = faqs }) {
+  return (
+    <div className="accordion">
+      {data.map((element, index) => (
+        <AccordionItem title={element.title} text={element.text} num={index} />
+      ))}
+    </div>
+  );
 }
