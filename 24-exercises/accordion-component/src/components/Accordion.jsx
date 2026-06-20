48177b4163cf09ed6b5903d309f2dce210ac680e
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 12:34:14 +0200

    fix: add keys to accordion items

--- 24-exercises/accordion-component/src/components/Accordion.jsx
+++ 24-exercises/accordion-component/src/components/Accordion.jsx
@@ -5,7 +5,12 @@ export default function Accordion({ data = faqs }) {
   return (
     <div className="accordion">
       {data.map((element, index) => (
-        <AccordionItem title={element.title} text={element.text} num={index} />
+        <AccordionItem
+          title={element.title}
+          text={element.text}
+          num={index}
+          key={index}
+        />
       ))}
     </div>
   );
