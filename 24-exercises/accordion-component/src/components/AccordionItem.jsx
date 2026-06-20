fd6c5a814637f375d6952a1d1097a6c19616051e
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-20 12:34:43 +0200

    feat: add accordion toggle functionality

--- 24-exercises/accordion-component/src/components/AccordionItem.jsx
+++ 24-exercises/accordion-component/src/components/AccordionItem.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className="item">
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
 
-      <div className="content-box">{text}</div>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
