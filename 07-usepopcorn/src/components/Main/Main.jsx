e4c792ee6ebafd3ec9556e0a50b1d74de53f6089
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-08-21 05:03:45 +0200

    feat: add Main layout container component

---
+++ 07-usepopcorn/src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import ListBox from "./ListBox";
+import WatchedBox from "./WatchedBox";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <ListBox />
+
+      <WatchedBox />
+    </main>
+  );
+}
