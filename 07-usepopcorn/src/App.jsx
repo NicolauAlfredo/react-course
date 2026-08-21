d30e14b2135a67a2cffef9d0b2a1b3942d61dfbc
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-08-21 05:03:47 +0200

    feat: assemble NavBar and Main in App component

---
+++ 07-usepopcorn/src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/Header/NavBar";
+import Main from "./components/Main/Main";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+
+      <Main />
+    </>
+  );
+}
